4adc1a679d6d7e7304170d67d9e39f1625376a0d
lambor590 <lambor590@gmail.com>
2024-03-30 18:39:09 +0100

    Actualizar dominio de descargas

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,7 @@ const { DistributionAPI } = require("helios-core/common");
 
 const ConfigManager = require("./configmanager");
 
-exports.REMOTE_DISTRO_URL = "https://otcr.tk/mc/distribution.json";
+exports.REMOTE_DISTRO_URL = "https://otko.pp.ua/mc/distribution.json";
 
 const api = new DistributionAPI(
   ConfigManager.getLauncherDirectory(),
